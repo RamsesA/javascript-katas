586cad2dd7cb644d79f0cbedf2b39fafe3afc76e
RamsesA <ralexander@pathfinderstudents.org>
2017-10-17 11:28:02 -0400

    Update 1-conditionals.js
    kata solved

--- 1-conditionals.js
+++ 1-conditionals.js
@@ -1,1 +1,20 @@
-/*  1.1 conditionals with prompt */
+/*  1.2 While Loop */
+
+// Conditional where flavor = chocolate or vanilla
+// Prompt for flavor, then output "here's your" + flavor
+// If strawberry, alert "all out of strawberry"
+// Else alert "no such flavor"
+
+scoop = prompt("What flavor would you like?");
+
+	if ( scoop == "chocolate" || scoop == "vanilla") {
+	alert("Here's your "+scoop);
+	}
+
+	else if ( scoop == "strawberry") {
+	alert("We're all out of "+scoop);
+	}
+
+	else{
+		alert("There is no such flavor.")
+	}
